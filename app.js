89445d03eb660450a7f11dcd23cd903a347471ac
Jake Sims <jake.jake.sims@gmail.com>
2022-02-28 11:15:18 -0600

    completed advanced js drills

---
+++ app.js
@@ -0,0 +1,55 @@
+// name = 'Jake'; 
+// let name; 
+// console.log(name); 
+
+// name = 'Jake'; 
+// var name; 
+// console.log(name); 
+
+// let someArray = ['h1', 'h2', 'h3', 'H']; 
+//   let i = 0; 
+
+// recursion(); 
+// function recursion () {
+
+//     if (i < someArray.length) {
+//         console.log(someArray[i]); 
+//         i++
+//         recursion ()
+//     } else {
+
+//     }
+// }; 
+// console.log('test1'); 
+// let avg = findAverage(2, 2); 
+// console.log('test2', avg); 
+// function findAverage (a, b) {
+//     console.log('test3'); 
+//     var answer = (a+b) / 2; 
+//     return answer; 
+
+
+// }
+
+// let fruits = ['pear', 'watermelon', 'apple'];
+// let favFruit; 
+// function printFruits() {
+//     let favFruit = fruits[1];
+//     console.log(fruits[2]);
+//     printFavFruit()
+//     console.log(leastFavFruit); 
+//     function printFavFruit () { 
+//     let leastFavFruit = fruits[0];
+     
+//     console.log(favFruit); 
+// }
+// }; 
+
+
+// printFruits(); 
+
+
+let func = function greetingFunc () {
+    console.log('hello'); 
+}
+func (); 
